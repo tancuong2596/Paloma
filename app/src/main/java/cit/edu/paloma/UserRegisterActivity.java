96c0153d00224cbcd264c22c594ec9e38199e152
Nguyễn Tấn Cường <cuongb1400682@student.ctu.edu.vn>
2017-02-23 11:15:48 +0700

    add 'UserRegisterUtils.java'. add 'initViews()' method in 'UserRegisterActivity' class

--- app/src/main/java/cit/edu/paloma/UserRegisterActivity.java
+++ app/src/main/java/cit/edu/paloma/UserRegisterActivity.java
@@ -2,12 +2,26 @@ package cit.edu.paloma;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class UserRegisterActivity extends AppCompatActivity {
+
+    private EditText userFullNameEdit;
+    private EditText emailEdit;
+    private EditText passEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
+        initViews();
     }
+
+    private void initViews() {
+        userFullNameEdit = (EditText) findViewById(R.id.register_full_name_edit);
+        emailEdit = (EditText) findViewById(R.id.register_email_edit);
+        passEdit = (EditText) findViewById(R.id.register_pass_edit);
+    }
+
+
 }
---
+++ app/src/main/java/cit/edu/paloma/utils/UserRegisterUtils.java
@@ -0,0 +1,9 @@
+package cit.edu.paloma.utils;
+
+/**
+ * Created by charlie on 2/23/17.
+ */
+
+public class UserRegisterUtils {
+
+}
