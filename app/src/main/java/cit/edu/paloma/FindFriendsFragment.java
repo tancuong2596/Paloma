19715b6e5bf47528c45a3d8a48b81a615c19669f
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-03-07 11:42:08 +0700

    add FragmentFindFriends, add ic_back.png

---
+++ app/src/main/java/cit/edu/paloma/FindFriendsFragment.java
@@ -0,0 +1,19 @@
+package cit.edu.paloma;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FindFriendsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_find_friends, container, false);
+    }
+
+}
