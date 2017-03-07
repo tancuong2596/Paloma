ec1009b4639d969a6748cc1a22774f3861d624b3
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-03-07 11:52:18 +0700

    add list view to show suggested friends on FindFriendsFragment

--- app/src/main/java/cit/edu/paloma/FindFriendsFragment.java
+++ app/src/main/java/cit/edu/paloma/FindFriendsFragment.java
@@ -7,13 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 public class FindFriendsFragment extends Fragment {
+    private View rootView;
+    private ListView mSuggestedFriendsList;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_find_friends, container, false);
+        rootView = inflater.inflate(R.layout.fragment_find_friends, container, false);
+        initViews();
+        return rootView;
     }
 
+    private void initViews() {
+        mSuggestedFriendsList = (ListView) rootView.findViewById(R.id.suggested_friends_list);
+    }
 }
