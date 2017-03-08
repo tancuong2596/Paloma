58b48bada08ed31e48888007e5ccaa941dbcbcfc
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-03-08 08:48:17 +0700

    add online/offline indicator

--- app/src/main/java/cit/edu/paloma/FindFriendsFragment.java
+++ app/src/main/java/cit/edu/paloma/FindFriendsFragment.java
@@ -7,12 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import cit.edu.paloma.adapters.FriendListAdapter;
+import cit.edu.paloma.datamodals.User;
 
 
 public class FindFriendsFragment extends Fragment {
     private View rootView;
     private ListView mSuggestedFriendsList;
+    private FriendListAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,7 +27,14 @@ public class FindFriendsFragment extends Fragment {
         return rootView;
     }
 
+    public void addSuggestedFriend(User friend) {
+        
+    }
+
     private void initViews() {
+        mAdapter = new FriendListAdapter(getContext());
+
         mSuggestedFriendsList = (ListView) rootView.findViewById(R.id.suggested_friends_list);
+        mSuggestedFriendsList.setAdapter(mAdapter);
     }
 }
