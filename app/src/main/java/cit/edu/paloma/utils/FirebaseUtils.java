e9a621ed0a9fbf0f3d3db2cde2b8d6029eab9d7e
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-03-12 20:08:28 +0700

    add FirebaseUtils.createNewChatGroup()

--- app/src/main/java/cit/edu/paloma/utils/FirebaseUtils.java
+++ app/src/main/java/cit/edu/paloma/utils/FirebaseUtils.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.util.StringBuilderPrinter;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -12,10 +13,16 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 
+import cit.edu.paloma.datamodals.ChatGroup;
 import cit.edu.paloma.datamodals.User;
 
 public class FirebaseUtils {
@@ -27,6 +34,10 @@ public class FirebaseUtils {
 
     public static DatabaseReference getUsersRef() {
         return FirebaseDatabase.getInstance().getReference().child("users");
+    }
+
+    public static DatabaseReference getChatGroupsRef() {
+        return FirebaseDatabase.getInstance().getReference().child("chatGroups");
     }
 
     public static void updateUsersChildren(DatabaseReference userRef,
@@ -41,4 +52,36 @@ public class FirebaseUtils {
                 .updateChildren(updateChildren, completionListener);
     }
 
+    public static DatabaseReference createNewChatGroup(final ArrayList<Object[]> members, @Nullable OnCompleteListener onCompleteListener) {
+        DatabaseReference chatGroupRef = FirebaseUtils
+                .getChatGroupsRef()
+                .push();
+
+        String groupChatId = chatGroupRef.getKey();
+        StringBuilder groupNameBuilder = new StringBuilder();
+        ArrayList<String> groupMembersUid = new ArrayList<>();
+
+        for (Object[] member : members) {
+            User user = (User) member[1];
+            groupNameBuilder.append(user.getFullName()).append(", ");
+            groupMembersUid.add(user.getUserId());
+        }
+
+        ChatGroup chatGroup = new ChatGroup(
+                groupChatId,
+                groupNameBuilder.substring(0, groupNameBuilder.length() - 3),
+                groupMembersUid,
+                Collections.emptyList(),
+                System.currentTimeMillis()
+        );
+
+        HashMap<String, Object> updateChildren = new HashMap<>();
+        updateChildren.put(groupChatId, chatGroup.toMap());
+
+        FirebaseUtils
+                .getChatGroupsRef()
+                .updateChildren(updateChildren);
+
+        return chatGroupRef;
+    }
 }
