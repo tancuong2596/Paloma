26aa52336933ae4e0c0b501dc1dce160ae3fef5c
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-03-12 11:01:35 +0700

    add findUsersWithPattern(String pattern) in FindFriendsFragment remove setListOfUsers(@Nullable ArrayList<Object[]>) from FindFriendsFragment

--- app/src/main/java/cit/edu/paloma/fragments/FindFriendsFragment.java
+++ app/src/main/java/cit/edu/paloma/fragments/FindFriendsFragment.java
@@ -14,7 +14,13 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -22,6 +28,7 @@ import java.util.Collections;
 import cit.edu.paloma.R;
 import cit.edu.paloma.adapters.SuggestedFriendListAdapter;
 import cit.edu.paloma.datamodals.User;
+import cit.edu.paloma.utils.FirebaseUtils;
 
 
 public class FindFriendsFragment extends Fragment {
@@ -50,28 +57,59 @@ public class FindFriendsFragment extends Fragment {
         mSearchingText = (TextView) rootView.findViewById(R.id.searching_text);
     }
 
-    public void setListOfUsers(@Nullable ArrayList<Object[]> listOfUsers) {
-        if (listOfUsers == null) {
-            listOfUsers = (ArrayList<Object[]>) Collections.<Object[]>emptyList();
-        }
-
-        mAdapter = new SuggestedFriendListAdapter(getContext());
-        mAdapter.addAll(listOfUsers);
-        mSuggestedFriendsList.setAdapter(mAdapter);
-        mAdapter.notifyDataSetChanged();
-
-        if (listOfUsers.isEmpty()) {
-            mNoResultText.setVisibility(View.VISIBLE);
-        } else {
-            mNoResultText.setVisibility(View.GONE);
-        }
-    }
-
     public void showProgressBar(boolean b) {
         if (b) {
             mSearchingText.setVisibility(View.VISIBLE);
+            mNoResultText.setVisibility(View.GONE);
         } else {
             mSearchingText.setVisibility(View.GONE);
+            if (mAdapter.isEmpty()) {
+                mNoResultText.setVisibility(View.VISIBLE);
+            } else {
+                mNoResultText.setVisibility(View.GONE);
+            }
         }
+    }
+
+    public void findUsersWithPattern(final String pattern) {
+        if (mSearchingText.getVisibility() == View.VISIBLE) {
+            return;
+        }
+
+        showProgressBar(true);
+        mAdapter.clear();
+        mAdapter.notifyDataSetChanged();
+
+        final FirebaseUser firebaseCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        FirebaseUtils
+                .getUsersRef()
+                .orderByChild("email")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            User user = snapshot.getValue(User.class);
+                            String email = user.getEmail().toLowerCase();
+
+                            if (email.contains(pattern)) {
+                                if (!email.equalsIgnoreCase(firebaseCurrentUser.getEmail())) {
+                                    mAdapter.add(new Object[]{snapshot.getRef(), user});
+                                }
+                            }
+
+                        }
+
+                        mAdapter.notifyDataSetChanged();
+                        showProgressBar(false);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        mAdapter.clear();
+                        mAdapter.notifyDataSetChanged();
+                        showProgressBar(false);
+                    }
+                });
     }
 }
