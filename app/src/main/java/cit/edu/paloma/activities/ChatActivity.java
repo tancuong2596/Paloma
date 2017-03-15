20558614becdef94162fefd526f6b66c9f2988ac
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-03-15 11:53:35 +0700

    add ChatActivity

---
+++ app/src/main/java/cit/edu/paloma/activities/ChatActivity.java
@@ -0,0 +1,15 @@
+package cit.edu.paloma.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cit.edu.paloma.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
