c5e066049bd89eb133353be697abad7e6d1c7ec7
Nguyễn Tấn Cường <tancuong2596@gmail.com>
2017-03-17 09:02:35 +0700

    extend MessagesListAdapter from BaseAdapter

--- app/src/main/java/cit/edu/paloma/adapters/MessagesListAdapter.java
+++ app/src/main/java/cit/edu/paloma/adapters/MessagesListAdapter.java
@@ -3,18 +3,87 @@ package cit.edu.paloma.adapters;
 import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import cit.edu.paloma.datamodals.Message;
+import cit.edu.paloma.utils.FirebaseUtils;
 
 /**
  * Created by charlie on 3/15/17.
  */
 
-public class MessagesListAdapter extends ArrayAdapter<Message> {
+public class MessagesListAdapter extends BaseAdapter {
+    private final Context mContext;
+    private final ArrayList<Message> mMessagesList;
+    private final HashMap<Message, Integer> mMessageIndexMap;
+
     public MessagesListAdapter(@NonNull Context context) {
-        super(context, 0, new ArrayList<Message>());
+        this.mContext = context;
+        this.mMessagesList = new ArrayList<>();
+        this.mMessageIndexMap = new HashMap<>();
+        setupMessagesChildEventListener();
+    }
+
+    private void setupMessagesChildEventListener() {
+        FirebaseUtils
+                .getMessagesRef()
+                .orderByChild("timestamp")
+                .addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    @Override
+    public int getCount() {
+        return mMessagesList.size();
+    }
+
+    @Override
+    public Message getItem(int position) {
+        return mMessagesList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
     }
 }
